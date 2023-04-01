9ee3cf9b9e4bc26625d09f74731bceac3079b134
brightalot <gamja9151@hanyang.ac.kr>
2023-04-01 23:51:23 +0900

    imagePreview test

--- app/src/main/java/com/example/happybirthday2/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,9 +14,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday2.ui.theme.HappyBirthday2Theme
+import androidx.compose.foundation.Image
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,6 +55,15 @@ fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier =
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthday2Theme {
-        BirthdayGreetingWithText(message = "Happy Birthday Hyunsu", from="- from gamja")
+        BirthdayGreetingWithImage("Happy Birthday Hyunsu", "- from gamja")
     }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+        )
 }
